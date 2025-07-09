32f76a52e32f8cb93766585bbc970064bd56023b
NPGiakhiem <Khiemnpgce182252@fpt.edu.vn>
2025-07-09 19:37:31 +0700

    Add AboutUsAndHowToPlay

---
+++ Menu_Game/HowToPlay.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Menu_Game
+{
+    /// <summary>
+    /// Interaction logic for HowToPlay.xaml
+    /// </summary>
+    public partial class HowToPlay : Window
+    {
+        public HowToPlay()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MenuGame mainWindow = new MenuGame();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
