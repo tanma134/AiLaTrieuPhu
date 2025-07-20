f8a794d52c1719160c1a7d9ff44a94648cef1483
TaiNHCE <TaiNHCE181569@fpt.edu.vn>
2025-07-20 14:48:37 +0700

    fix login

--- AiLaTrieuPhu_Account/obj/Debug/net8.0-windows/App.g.i.cs
+++ AiLaTrieuPhu_Account/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace AiLaTrieuPhu_Account {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AiLaTrieuPhu_Account {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public static void Main() {
             AiLaTrieuPhu_Account.App app = new AiLaTrieuPhu_Account.App();
             app.InitializeComponent();
