1c270e88e1fab606f68b8f6a723ceffc6b6c8ce2
Gia Bảo Hồ <kknbaonkk@gmail.com>
2025-07-21 16:25:40 +0700

    Fix Music

--- AiLaTrieuPhu_DEMO/obj/Debug/net8.0-windows/App.g.cs
+++ AiLaTrieuPhu_DEMO/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace AiLaTrieuPhu_DEMO {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AiLaTrieuPhu_DEMO {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public static void Main() {
             AiLaTrieuPhu_DEMO.App app = new AiLaTrieuPhu_DEMO.App();
             app.InitializeComponent();
